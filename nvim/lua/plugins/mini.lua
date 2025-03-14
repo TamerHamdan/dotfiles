ebccdc080bf4ba340fd1291a9409fda2f1009f56
TamerHamdan <dev.tamerh@gmail.com>
2025-03-14 02:05:08 +0200

    updatedV1

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -1,34 +1,34 @@
 return {
-    { -- Collection of various small independent plugins/modules
-        'echasnovski/mini.nvim',
-        config = function()
-            -- Better Around/Inside textobjects
-            --
-            -- Examples:
-            --  - va)  - [V]isually select [A]round [)]paren
-            --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-            --  - ci'  - [C]hange [I]nside [']quote
-            require('mini.ai').setup { n_lines = 500 }
+	{ -- Collection of various small independent plugins/modules
+		"echasnovski/mini.nvim",
+		config = function()
+			-- Better Around/Inside textobjects
+			--
+			-- Examples:
+			--  - va)  - [V]isually select [A]round [)]paren
+			--  - yinq - [Y]ank [I]nside [N]ext [Q]uote
+			--  - ci'  - [C]hange [I]nside [']quote
+			require("mini.ai").setup({ n_lines = 500 })
 
-            -- Add/delete/replace surroundings (brackets, quotes, etc.)
-            --
-            -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-            -- - sd'   - [S]urround [D]elete [']quotes
-            -- - sr)'  - [S]urround [R]eplace [)] [']
-            require('mini.surround').setup()
+			-- Add/delete/replace surroundings (brackets, quotes, etc.)
+			--
+			-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+			-- - sd'   - [S]urround [D]elete [']quotes
+			-- - sr)'  - [S]urround [R]eplace [)] [']
+			require("mini.surround").setup()
 
-            -- Simple and easy statusline.
-            local statusline = require 'mini.statusline'
-            statusline.setup { use_icons = vim.g.have_nerd_font }
+			-- Simple and easy statusline.
+			local statusline = require("mini.statusline")
+			statusline.setup({ use_icons = vim.g.have_nerd_font })
 
-            -- You can configure sections in the statusline by overriding their
-            -- default behavior. For example, here we set the section for
-            -- cursor location to LINE:COLUMN
-            statusline.section_location = function()
-                return '%2l:%-2v'
-            end
+			-- You can configure sections in the statusline by overriding their
+			-- default behavior. For example, here we set the section for
+			-- cursor location to LINE:COLUMN
+			statusline.section_location = function()
+				return "%2l:%-2v"
+			end
 
-            --  Check out: https://github.com/echasnovski/mini.nvim
-        end,
-    },
+			--  Check out: https://github.com/echasnovski/mini.nvim
+		end,
+	},
 }
