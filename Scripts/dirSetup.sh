18b1e373c86872f022c1ecb7c918f1d72b6aecb9
TamerHamdan <dev.tamerh@gmail.com>
2025-03-31 18:09:04 +0300

    dirSetup.sh fixed removing original script dir

--- Scripts/dirSetup.sh
+++ Scripts/dirSetup.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
-set -e
 
-if [ -d ~/Developer ]; then
-    echo "Warning: ~/Developer already exists. Continue? (y/N)"
-    read -r response
-    if [[ ! $response =~ ^[Yy]$ ]]; then
-        echo "Operation cancelled"
-        exit 0
-    fi
-fi
-
+# Make Developer directories
 mkdir -p ~/Developer/{Temp,Repos,.Archive,Uni,Working,Scripts}
 echo "Developer directory structure created"
 
-cp "$(realpath "$0")" ~/Developer/Scripts/
-echo "Script folder copied to ~/Developer/Scripts/" 
+# Copy scripts from dotfiles to Developer
+cp -r ~/dotfiles/Scripts/* ~/Developer/Scripts/
+
+# Remove original scripts folder
+rm -rf ~/dotfiles/Scripts
+
+echo "Done! Developer folder setup complete."
