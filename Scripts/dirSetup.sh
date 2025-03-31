0d67106dc38f7bf10b640b4943e2d4b5ffb67a0b
TamerHamdan <dev.tamerh@gmail.com>
2025-03-31 18:14:56 +0300

    fixed dirsetup.sh

--- Scripts/dirSetup.sh
+++ Scripts/dirSetup.sh
@@ -5,9 +5,9 @@ mkdir -p ~/Developer/{Temp,Repos,.Archive,Uni,Working,Scripts}
 echo "Developer directory structure created"
 
 # Copy scripts from dotfiles to Developer
-cp -r ~/dotfiles/Scripts/* ~/Developer/Scripts/
+cp -r ~/Scripts/* ~/Developer/Scripts/
 
 # Remove original scripts folder
-rm -rf ~/dotfiles/Scripts
+rm -rf ~/Scripts
 
 echo "Done! Developer folder setup complete."
