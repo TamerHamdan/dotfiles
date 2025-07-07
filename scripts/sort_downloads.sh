9078de142b1ddb8d14bf2696cf00594fca5d76f3
TH <177368717+buzzedowl@users.noreply.github.com>
2025-07-07 18:01:02 +0300

    small update

--- scripts/sort_downloads.sh
+++ scripts/sort_downloads.sh
@@ -6,43 +6,37 @@ DOWNLOADS="$HOME/Downloads"
 cd "$DOWNLOADS" || exit 1
 
 # Create category folders
-mkdir -p images videos documents audio archives applications code books other
+mkdir -p media documents archives applications other
 
-# Move files based on extensions
-for file in *.*; do
-    [ -f "$file" ] || continue
+# Function to process a single file
+process_file() {
+    local file="$1"
+    [ -f "$file" ] || return
     
     case "${file##*.}" in
-        # Images
-        jpg|jpeg|png|gif|bmp|tiff|webp|svg|ico|heic) mv "$file" images/ ;;
-        
-        # Videos
-        mp4|avi|mkv|mov|wmv|flv|webm|m4v|3gp) mv "$file" videos/ ;;
+        # Media (Images and Videos)
+        "jpg"|"jpeg"|"png"|"gif"|"bmp"|"tiff"|"webp"|"svg"|"ico"|"heic"|"mp4"|"avi"|"mkv"|"mov"|"wmv"|"flv"|"webm"|"m4v"|"3gp") mv "$file" media/ ;;
         
         # Documents
-        pdf|doc|docx|txt|rtf|odt|pages|md|tex) mv "$file" documents/ ;;
-        
-        # Audio
-        mp3|wav|flac|aac|ogg|m4a|wma) mv "$file" audio/ ;;
+        "pdf"|"doc"|"docx"|"txt"|"rtf"|"odt"|"pages"|"md"|"tex") mv "$file" documents/ ;;
         
         # Archives
-        zip|rar|tar|gz|7z|bz2|xz|tgz) mv "$file" archives/ ;;
+        "zip"|"rar"|"tar"|"gz"|"7z"|"bz2"|"xz"|"tgz") mv "$file" archives/ ;;
         
         # Applications
-        dmg|pkg|deb|rpm|appimage|exe|msi) mv "$file" applications/ ;;
-        
-        # Code
-        py|js|html|css|php|java|cpp|c|sh|rb|go|rs) mv "$file" code/ ;;
-        
-        # Books
-        epub|mobi|azw|azw3) mv "$file" books/ ;;
+        "dmg"|"pkg"|"deb"|"rpm"|"appimage"|"exe"|"msi") mv "$file" applications/ ;;
         
         # Everything else (common unsorted files)
-        sav|save|dat|part|crdownload|tmp|log|cfg|ini|conf|bak|old|cache) mv "$file" other/ ;;
+        "sav"|"save"|"dat"|"part"|"crdownload"|"tmp"|"log"|"cfg"|"ini"|"conf"|"bak"|"old"|"cache") mv "$file" other/ ;;
         
         # Catch all remaining files
         *) mv "$file" other/ ;;
     esac
+}
+
+# Process all files recursively, excluding the category folders we just created
+find . -type f -not -path "./media/*" -not -path "./documents/*" -not -path "./archives/*" -not -path "./applications/*" -not -path "./other/*" | while read -r file; do
+    process_file "$file"
 done
 
 echo "Downloads folder organized!"
